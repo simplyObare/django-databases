ccfa0aa7361490d0ec6e36e45d2c8408aa03c3ad
simplyObare <josephatowiny@gmail.com>
2024-12-02 19:19:34 +0300

    working with models.py

--- djangodb/website/models.py
+++ djangodb/website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Members(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField()
+    passwd = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f"Name: {self.fname} {self.lname} Age: {self.age}"
