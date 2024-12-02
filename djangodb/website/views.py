21c3e0d8a448c69389fde44c5bdca2390c2ea52c
simplyObare <josephatowiny@gmail.com>
2024-12-03 02:41:09 +0300

    pulling data from the database

--- djangodb/website/views.py
+++ djangodb/website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Member
 
 
 # Create your views here.
 def home(request):
-    return render(request, "Home.html", {})
+    all_members = Member.objects.all()
+    return render(request, "Home.html", {"all": all_members})
